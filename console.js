49c1818bc000fdc0d88c749006a2a44ec560e263
Styles-theandriod <59735626+Styles-Theandriod@users.noreply.github.com>
2023-01-18 14:38:32 +0100

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,5 @@
 //This is a comment 
 console.log('this is a new file')
 console.log('this is a new file') // another comment
+console.log('this is a new file that was just added just now') // another comment
+
